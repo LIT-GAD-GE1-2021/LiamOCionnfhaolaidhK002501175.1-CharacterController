b371f1d1b998c219793c5c5a4dbe6597bcb63718
Liamabean <77436937+Liamabean@users.noreply.github.com>
2021-02-09 15:59:38 +0000

    summommy

---
+++ 5.1-CharacterController/Assets/Scripts/AdventurerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventurerScript : MonoBehaviour
+{
+    
+    public float jumpSpeed;
+    public float horizontalSpeed = 10;
+    public LayerMask whatIsGround;
+    public Transform groundcheck;
+    private bool grounded;
+    private bool jump;
+
+    bool facingRight = true;
+    private float hAxis;
+    private Rigidbody2D theRigidBody;
+    private Animator theAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
